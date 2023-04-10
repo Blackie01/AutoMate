b0022e6be48465e45d6b2b8ca15efa2363ffa9a0
Blackie01 <damola24795@gmail.com>
2023-04-10 23:47:05 +0100

    base homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Homepage from './components/Homepage';
+
+function App() {
+  return (
+    <section className="app">
+      <Homepage />
+
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/Homepage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Navigation from './Nav';
+import './Homepage.css'
+import jumboImage from '../assets/jumbo.jpg';
+import Button from '@mui/material/Button';
+
+
+function Homepage () {
+    
+    return (
+        <section>
+            
+            <Navigation />
+
+            <section className='hero'>
+                <section className='hero-text-area'>
+                    <h1 className='jumbo-text'>Manage your fleet, <br/> like a pro.</h1>
+                    <p>the number of vehicles don't matter, AutoMate provides all <br/> the tools you need to boost productivity</p>
+                
+                    <section className='jumbo-buttons'>
+                        <Button variant='outlined'>Get Started</Button>
+                        <Button variant='contained'>Download App</Button>
+                    </section>
+                
+                </section>
+
+                <section className='hero-image'>
+                    <img className='jumbo-image' src={jumboImage} alt='Image'/>
+                </section>
+            </section>
+        </section>
+    );
+}
+
+export default Homepage;
