c139536c2104c165903ddf8fa28d2ccdb5a28f5d
Blackie01 <damola24795@gmail.com>
2023-04-12 23:29:31 +0100

    homepage 25 percent

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -38,20 +38,98 @@ function Homepage () {
             <section className='hero-first-floor'>
                 <section className='hero-first-floor-image'>
                     <img className='driver-image' src={driverImage} alt='driver-Image'/>
+
+                    <section className='jumbo-buttons'>
+                        <Button variant='contained'>Get a quote</Button>
+                        <Button variant='outlined'>Learn more</Button>
+                    </section>
                 </section>
 
-               <aside>
-                    <section>
-                        <h2>Products & Services</h2>
+               <section className='hero-first-floor-text'>
+                    <section className='product-title-container'>
+                        <h2 className='product-title'>Products & Services</h2>
+                        <p className='product-title-subtext'>
+                            By trusting us with your fleet management, you enjoy the following. 
+                            <br/> 
+                            You just need to click a button.
+                        </p>
+                        <hr/>
                     </section>
 
-                    <section>
-                        <div>01</div>
-                        <section>
-                            
+                    <section className='products'>
+                        <h1 className='product-number'>01</h1>
+                        <section className='product-listing-container'>
+                            <h2 className='product-listing-title'>Driver Safety.</h2>
+                            <p className='product-listing-text'>
+                            Prevent accidents and exonerate drivers with the most accurate, 
+                            fastest AI dash cam. Reduce insurance costs by improving 
+                            unsafe driving habits through automated coaching.
+                            </p>
                         </section>
                     </section>
-               </aside>
+
+                    <section className='products'>
+                        <h1 className='product-number'>02</h1>
+                        <section className='product-listing-container'>
+                            <h2 className='product-listing-title'>Tracking & Telematics.</h2>
+                            <p className='product-listing-text'>
+                            Real-time visibility into the location, 
+                            utilization, and health of vehicles, equipment, and assets.
+                            </p>
+                        </section>
+                    </section>
+
+                    <section className='products'>
+                        <h1 className='product-number'>03</h1>
+                        <section className='product-listing-container'>
+                            <h2 className='product-listing-title'>Maintenance Scheduling.</h2>
+                            <p className='product-listing-text'>
+                            Maximize productivity and reduce costs with preventive fleet maintenance.
+                            Create thorough inspection records for vehicles and assets. 
+                            Maintain and access records electronically.
+                            </p>
+                        </section>
+                    </section>
+
+                    <section className='products'>
+                        <h1 className='product-number'>04</h1>
+                        <section className='product-listing-container'>
+                            <h2 className='product-listing-title'>Dispatch.</h2>
+                            <p className='product-listing-text'>
+                            Organize, assign, and optimize for on-time deliveries with cloud-based dispatch management. 
+                            Built on Motive’s high-powered tracking solution. 
+                            Dispatch your fleet with confidence.
+                            </p>
+                        </section>
+                    </section>
+
+                    <section className='products'>
+                        <h1 className='product-number'>05</h1>
+                        <section className='product-listing-container'>
+                            <h2 className='product-listing-title'>Fleet Sustainability.</h2>
+                            <p className='product-listing-text'>
+                            Save on fuel and reduce your environmental impact 
+                            with AI-powered insights and fleet fuel management.
+                            See your fleet’s objective fuel consumption and trends. 
+                            
+                            </p>
+                        </section>
+                    </section>
+
+                    <section className='products'>
+                        <h1 className='product-number'>06</h1>
+                        <section className='product-listing-container'>
+                            <h2 className='product-listing-title'>Dashcams.</h2>
+                            <p className='product-listing-text'>
+                            When a road event occurs, get the whole picture with the 
+                            smartest dual-facing camera on the market. SafetyCam video, 
+                            backed by the power of AutoMate, provides the tools you 
+                            need to keep your drivers on top of their game. 
+                            
+                            </p>
+                        </section>
+                    </section>
+               </section>
 
             </section>
         </section>
