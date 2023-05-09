a7cdc8befbe7859daf1a54203abd8b42da26f5ae
Blackie01 <onioludamola@gmail.com>
2023-05-09 21:47:24 +0100

    updates

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -8,6 +8,9 @@ import gabriel from "../assets/gabriel.jpg";
 import lois from "../assets/lois.jpg";
 
 function Testimonials() {
+
+  // object data
+
   const users = [
     {
       id: 1,
@@ -19,56 +22,58 @@ function Testimonials() {
         "AutoMate has transformed our operations by optimizing routes and reducing fuel consumption, resulting in significant cost savings.",
     },
 
-    // {
-    //   id: 2,
-    //   name: "Gbenga Ogunmade",
-    //   company: "Gbenga Motors",
-    //   userImage: gbenga,
-    //   position: "CEO",
-    //   words:
-    //     "With AutoMate real-time vehicle tracking, we are able to monitor our fleet performance and ensure on-time deliveries.",
-    // },
+    {
+      id: 2,
+      name: "Gbenga Ajibade",
+      company: "Gbenga Motors",
+      userImage: gbenga,
+      position: "CEO",
+      words:
+        "With AutoMate real-time vehicle tracking, we are able to monitor our fleet performance and ensure on-time deliveries.",
+    },
 
-    // {
-    //   id: 3,
-    //   name: "Godwin John",
-    //   company: "Bolton White Hotels",
-    //   userImage: godwin,
-    //   position: "Fleet Manager",
-    //   words:
-    //     "AutoMate maintenance scheduling feature has helped us streamline our maintenance processes and minimize downtime",
-    // },
+    {
+      id: 3,
+      name: "Godwin John",
+      company: "Bolton White Hotels",
+      userImage: godwin,
+      position: "Fleet Manager",
+      words:
+        "AutoMate maintenance scheduling feature has helped us streamline our maintenance processes and minimize downtime",
+    },
 
-    // {
-    //   id: 4,
-    //   name: "Adedoyin Gold",
-    //   company: "Motive Cars",
-    //   userImage: adedoyin,
-    //   position: "Domestic Fleet Coord",
-    //   words:
-    //     "AutoMate driver safety monitoring has helped us improve driver behavior and reduce the risk of accidents",
-    // },
+    {
+      id: 4,
+      name: "Adedoyin Gold",
+      company: "Motive Cars",
+      userImage: adedoyin,
+      position: "Domestic Fleet Coord",
+      words:
+        "AutoMate driver safety monitoring has helped us improve driver behavior and reduce the risk of accidents",
+    },
 
-    // {
-    //   id: 5,
-    //   name: "Gabriel Thompson",
-    //   company: "Julius Berger",
-    //   userImage: gabriel,
-    //   position: "Head of Logistics",
-    //   words:
-    //     "Thanks to AutoMate customizable reporting, we are able to access detailed fleet analytics and make informed decisions.",
-    // },
+    {
+      id: 5,
+      name: "Gabriel Thompson",
+      company: "Julius Berger",
+      userImage: gabriel,
+      position: "Head of Logistics",
+      words:
+        "Thanks to AutoMate customizable reporting, we are able to access detailed fleet analytics and make informed decisions.",
+    },
 
-    // {
-    //   id: 6,
-    //   name: "Lois Lane",
-    //   company: "Transformers Ltd.",
-    //   userImage: lois,
-    //   position: "CEO",
-    //   words:
-    //     "The AutoMate automated dispatch system has enabled us to efficiently assign tasks to our drivers and improve overall fleet productivity.",
-    // },
+    {
+      id: 6,
+      name: "Lois Lane",
+      company: "Transformers Ltd.",
+      userImage: lois,
+      position: "CEO",
+      words:
+        "The AutoMate automated dispatch system has enabled us to efficiently assign tasks to our drivers and improve overall fleet productivity.",
+    },
   ];
+
+  // mapping function
 
   const reviewCard = users.map((user) => {
     return (
@@ -91,6 +96,26 @@ function Testimonials() {
     );
   });
 
+// click events
+
+const userTwoClick = () => {
+//   document.getElementById('userOne').style.display = 'none'
+//   document.getElementById('userTwo').style.display = 'block'
+//   document.getElementById('userThree').style.display = 'none'
+//   document.getElementById('userFour').style.display = 'none'
+//   document.getElementById('userFive').style.display = 'none'
+//   document.getElementById('userSix').style.display = 'none'
+
+document.getElementsByClassName('.individual-container:nth-child(1)').style.display = 'none'
+document.getElementsByClassName('.individual-container:nth-child(2)').style.display = 'block'
+document.getElementsByClassName('.individual-container:nth-child(3)').style.display = 'none'
+document.getElementsByClassName('.individual-container:nth-child(4)').style.display = 'none'
+document.getElementsByClassName('.individual-container:nth-child(5)').style.display = 'none'
+document.getElementsByClassName('.individual-container:nth-child(6)').style.display = 'none'
+
+}
+
+
   return (
     <main className="container">
       <div className="title">
@@ -102,32 +127,32 @@ function Testimonials() {
       <section className="review-container">{reviewCard}</section>
 
       <section className="indicators">
-        <div className="indicator-content">
+        <div id="userOne" className="indicator-content">
           <img src="https://images.pexels.com/photos/1337384/pexels-photo-1337384.jpeg?auto=compress&cs=tinysrgb&w=800"/>
           <p>ODY Logistics</p>
         </div>
 
-        <div className="indicator-content">
+        <div id="userTwo" onClick={userTwoClick} className="indicator-content">
           <img src="https://images.pexels.com/photos/1162361/pexels-photo-1162361.jpeg?auto=compress&cs=tinysrgb&w=800"/>
           <p>Gbenga Motors</p>
         </div>
 
-        <div className="indicator-content">
+        <div id="userThree" className="indicator-content">
           <img src="https://images.pexels.com/photos/2235130/pexels-photo-2235130.jpeg?auto=compress&cs=tinysrgb&w=800"/>
           <p>Bolton White Hotels</p>
         </div>
 
-        <div className="indicator-content">
+        <div id="userFour" className="indicator-content">
           <img src="https://images.pexels.com/photos/3767673/pexels-photo-3767673.jpeg?auto=compress&cs=tinysrgb&w=800"/>
           <p>Motive Cars</p>
         </div>
 
-        <div className="indicator-content">
+        <div id="userFive" className="indicator-content">
           <img src="https://images.pexels.com/photos/570047/pexels-photo-570047.jpeg?auto=compress&cs=tinysrgb&w=800"/>
           <p>Julius Berger</p>
         </div>
 
-        <div className="indicator-content">
+        <div id="userSix" className="indicator-content">
           <img src="https://images.pexels.com/photos/1365795/pexels-photo-1365795.jpeg?auto=compress&cs=tinysrgb&w=800"/>
           <p>Transformers ltd</p>
         </div>
