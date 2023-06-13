9bd9ee1c12079bb546d1c84031872db204a18ced
Blackie01 <onioludamola@gmail.com>
2023-06-14 00:32:51 +0100

    testing

--- src/components/login.js
+++ src/components/login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { auth, provider } from "./authentication/config";
 import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
+import GoogleLogo from "../assets/rrgoogle.png";
 
 function Login() {
   const navigate = useNavigate();
@@ -34,30 +35,35 @@ function Login() {
 
   return (
     <section>
-      <Link to="/">
-        <button>go to home</button>
-      </Link>
-      <form>
-        <input
-          type="email"
-          placeholder="email"
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
+      <section className="login-container">
+        <Link to="/">
+          <button>go to home</button>
+        </Link>
+        <form>
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
 
-        <input
-          type="password"
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
 
-        <p id="error-display"></p>
+          <p id="error-display"></p>
 
-        <input type="submit" onClick={login} />
+          <input type="submit" onClick={login} />
 
-        <section onClick={signInWithGoogle}>Sign in with Google</section>
-      </form>
+          <section className="google-signin-button" onClick={signInWithGoogle}>
+            <img className="google-signin-img" src={GoogleLogo} />
+            <span>Sign in with Google</span>
+          </section>
+        </form>
+      </section>
     </section>
   );
 }
