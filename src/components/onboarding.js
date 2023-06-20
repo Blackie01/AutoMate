844e29421a99d282a4e313c046cca6d8e35b2c2e
Blackie01 <onioludamola@gmail.com>
2023-06-20 04:27:34 +0100

    onboarding one

--- src/components/onboarding.js
+++ src/components/onboarding.js
@@ -2,6 +2,14 @@ import React from "react";
 import { useNavigate } from "react-router";
 import { signOut } from "firebase/auth";
 import { auth } from "./authentication/config";
+import logo from '../assets/logo.png'
+import location from '../assets/xlocation.png'
+import maintenance from '../assets/maintenance.png'
+import dashcam from '../assets/dashcam.png'
+import fuel from '../assets/fuel.png'
+import dispatch from '../assets/dispatch.png'
+import telematics from '../assets/telematics.png'
+import './onboarding.css'
 
 
 function Onboarding () {
@@ -26,12 +34,57 @@ function Onboarding () {
 
     return (
 
-        <section>
-            oops! page under construction.
-            <button 
+        <section id="onboarding-one">
+            <div 
             // onClick={logout}
             onClick={logOut}
-            >Logout</button>
+            >
+              <img className="class-one-logo" src={logo} />
+            </div>
+
+            <section id="overall-onboard-one-container">
+              <h3 id="onboard-one-text">
+                Welcome, what do you need our help with? 
+              </h3>
+
+              <section id="onboard-one-options">
+                <div className="option-container">
+                  <img src={location} className="icons"/>
+                  <p>Vehicle Tracker</p>
+                </div>
+
+                <div className="option-container">
+                  <img src={maintenance} id="img-2-onboard" className="icons"/>
+                  <p>Maintenance</p>
+                </div>
+
+                <div className="option-container">
+                  <img src={dashcam} className="icons"/>
+                  <p>Dashcams</p>
+                </div>
+
+                <div className="option-container">
+                  <img src={fuel} className="icons"/>
+                  <p>Fuel Manager</p>
+                </div>
+
+                <div className="option-container">
+                  <img src={dispatch} className="icons"/>
+                  <p>Dispatch Control</p>
+                </div>
+
+                <div className="option-container">
+                  <img src={telematics} className="icons"/>
+                  <p>Telematics</p>
+                </div>
+              </section>
+
+              <hr id="onboard-hr"></hr>
+
+              <div className="next-button">Next</div>
+            </section>
+
+
         </section>
 
     )
